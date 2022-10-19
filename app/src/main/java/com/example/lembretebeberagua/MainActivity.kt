1449ff45cfacf1f433cf61dbc444219279d70167
Jefferson Douglas <86518315+jeffitando@users.noreply.github.com>
2022-10-19 14:54:39 -0300

    lembrete adicionado

--- app/src/main/java/com/example/lembretebeberagua/MainActivity.kt
+++ app/src/main/java/com/example/lembretebeberagua/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.lembretebeberagua
 
+import android.annotation.SuppressLint
+import android.app.TimePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.provider.AlarmClock
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import com.example.lembretebeberagua.model.CalcularIngestaoDiaria
 import java.text.NumberFormat
@@ -20,9 +20,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var txt_result: TextView
     private lateinit var ic_refresh: ImageView
 
+    private lateinit var bt_lembrete: Button
+    private lateinit var bt_alarme: Button
+    private lateinit var txt_hora: TextView
+    private lateinit var txt_minutos: TextView
+
     private lateinit var calcularIngestaoDiaria: CalcularIngestaoDiaria
     private var resultadoML = 0.0
 
+    lateinit var timePickerDialog: TimePickerDialog
+    lateinit var calendar: Calendar
+    var horaAtual = 0
+    var minutosAtuais = 0
+
+    @SuppressLint("QueryPermissionsNeeded")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -64,14 +75,49 @@ class MainActivity : AppCompatActivity() {
             dialog.show()
         }
 
+        bt_lembrete.setOnClickListener {
+
+            calendar = Calendar.getInstance()
+            horaAtual = calendar.get(Calendar.HOUR_OF_DAY)
+            minutosAtuais = calendar.get(Calendar.MINUTE)
+            timePickerDialog =
+                TimePickerDialog(this, { timePicker: TimePicker, hourOfDay: Int, minutes: Int ->
+                    txt_hora.text = String.format("%02d", hourOfDay)
+                    txt_minutos.text = String.format("%02d", minutes)
+                }, horaAtual, minutosAtuais, true)
+            timePickerDialog.show()
+        }
+
+        bt_alarme.setOnClickListener {
+            if (!txt_hora.text.toString().isEmpty() && !txt_minutos.text.toString().isEmpty()) {
+
+                val intent = Intent(AlarmClock.ACTION_SET_ALARM)
+                intent.putExtra(AlarmClock.EXTRA_HOUR, txt_hora.text.toString().toInt())
+                intent.putExtra(AlarmClock.EXTRA_MINUTES, txt_minutos.text.toString().toInt())
+                intent.putExtra(AlarmClock.EXTRA_MESSAGE, getString(R.string.alarme_mensagem))
+                startActivity(intent)
+
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+
+                }
+            }
+        }
+
     }
 
     private fun IniciarComponentes() {
+
         edit_peso = findViewById(R.id.edit_peso)
         edit_idade = findViewById(R.id.edit_idade)
         bt_calcular = findViewById(R.id.bt_calcular)
         txt_result = findViewById(R.id.txt_result)
         ic_refresh = findViewById(R.id.ic_refresh)
+        bt_lembrete = findViewById(R.id.bt_lembrete)
+        bt_alarme = findViewById(R.id.bt_alarme)
+        txt_hora = findViewById(R.id.txt_hora)
+        txt_minutos = findViewById(R.id.txt_minutos)
+
     }
 
 }
