d0bc47b1306d9eb3e38317104ce238d40d0f9135
Jefferson Douglas <86518315+jeffitando@users.noreply.github.com>
2022-10-19 11:40:31 -0300

    Calcular Ingestão diaria criada

--- app/src/main/java/com/example/lembretebeberagua/MainActivity.kt
+++ app/src/main/java/com/example/lembretebeberagua/MainActivity.kt
@@ -7,14 +7,21 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.lembretebeberagua.model.CalcularIngestaoDiaria
+import java.text.NumberFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var edit_peso:EditText
-    private lateinit var edit_idade:EditText
-    private lateinit var bt_calcular:Button
-    private lateinit var txt_result:TextView
-    private lateinit var ic_refresh:ImageView
+    private lateinit var edit_peso: EditText
+    private lateinit var edit_idade: EditText
+    private lateinit var bt_calcular: Button
+    private lateinit var txt_result: TextView
+    private lateinit var ic_refresh: ImageView
+
+    private lateinit var calcularIngestaoDiaria: CalcularIngestaoDiaria
+    private var resultadoML = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +29,44 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar!!.hide()
         IniciarComponentes()
+        calcularIngestaoDiaria = CalcularIngestaoDiaria()
 
         bt_calcular.setOnClickListener {
 
-            if (edit_peso.text.toString().isEmpty()){
-                Toast.makeText(this,R.string.toast_informe_peso, Toast.LENGTH_SHORT).show()
-            }else if (edit_idade.text.toString().isEmpty()){
-                Toast.makeText(this,R.string.toast_informe_idade, Toast.LENGTH_SHORT).show()
-            }else{
+            if (edit_peso.text.toString().isEmpty()) {
+                Toast.makeText(this, R.string.toast_informe_peso, Toast.LENGTH_SHORT).show()
+            } else if (edit_idade.text.toString().isEmpty()) {
+                Toast.makeText(this, R.string.toast_informe_idade, Toast.LENGTH_SHORT).show()
+            } else {
                 val peso = edit_peso.text.toString().toDouble()
                 val idade = edit_idade.text.toString().toInt()
+                calcularIngestaoDiaria.CalcularTotalMl(peso, idade)
+                resultadoML = calcularIngestaoDiaria.ResultadoML()
+                val formatar = NumberFormat.getNumberInstance(Locale("pt", "BR"))
+                formatar.isGroupingUsed = false
+                txt_result.text = formatar.format(resultadoML) + " " + "ml"
             }
+        }
+
+        ic_refresh.setOnClickListener {
+            val alertDialog = AlertDialog.Builder(this)
+            alertDialog.setTitle(R.string.dialog_titulo)
+                .setMessage(R.string.dialog_desc)
+                .setPositiveButton("OK") { dialogInterface, i ->
+                    edit_peso.setText("")
+                    edit_idade.setText("")
+                    txt_result.text = ""
+                }
+            alertDialog.setNegativeButton("Cancelar") { dialogInterface, i ->
+
+            }
+            val dialog = alertDialog.create()
+            dialog.show()
         }
 
     }
 
-    private fun IniciarComponentes(){
+    private fun IniciarComponentes() {
         edit_peso = findViewById(R.id.edit_peso)
         edit_idade = findViewById(R.id.edit_idade)
         bt_calcular = findViewById(R.id.bt_calcular)
--- app/src/main/java/com/example/lembretebeberagua/model/CalcularIngestaoDiaria.kt
+++ app/src/main/java/com/example/lembretebeberagua/model/CalcularIngestaoDiaria.kt
@@ -1,4 +1,34 @@
 package com.example.lembretebeberagua.model
 
 class CalcularIngestaoDiaria {
+    private val ML_JOVEM = 40.0
+    private val ML_ADULTO = 35.0
+    private val ML_IDOSO = 30.0
+    private val ML_MAIS_DE_66_ANOS = 25.0
+
+    private var resultML = 0.0
+    private var resultTotal = 0.0
+
+    fun CalcularTotalMl(peso: Double, idade: Int) {
+
+        if (idade <= 17) {
+            resultML = peso * ML_JOVEM
+            resultTotal = resultML
+
+        } else if (idade <= 55) {
+            resultML = peso * ML_ADULTO
+            resultTotal = resultML
+        } else if (idade <= 65) {
+            resultML = peso * ML_IDOSO
+            resultTotal = resultML
+        }else {
+            resultML = peso * ML_MAIS_DE_66_ANOS
+            resultTotal = resultML
+
+        }
+    }
+
+    fun ResultadoML(): Double{
+        return resultTotal
+    }
 }
